cc6920fa5a1b925691380de351b4d0e70b0d2d5c
Abdulrahman Hanafy <abdulrahman.elhanafy.dev@gmail.com>
2025-12-31 00:52:47 +0200

    trying to fix github pages base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: "/ICPC-Tanta-Community-TEST/",
+        base: '/ICPC-Tanta-Community-TEST/',
         server: {
             port: 3000,
             host: "0.0.0.0",
